676728e04516bd3b51a47c7b893768c823ede115
Taco <taco-tues@tuta.io>
2019-11-20 16:52:49 -0700

    onclick attempt is broken

--- main.js
+++ main.js
@@ -3,11 +3,29 @@ const nouns      =  words.nouns;
 
 const random_index = (array) =>  { return Math.floor(Math.random() * array.length) };
 
+function copy_text_to_clipboard(anchor_id) {
+  /* Get the text field */
+  const copy_text = document.getElementById(anchor_id);
+
+  /* Select the text field */
+  copy_text.select(null===anchor_id ? ' ' : anchor_id);
+  copy_text.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copy_text.value);
+}
+
 const create_anchor_list_item_w_last_pair = () => {
   const last_pair = document.getElementById("random_name_textarea").innerHTML;
   const anchor = document.createElement("A");
   anchor.id = last_pair.replace(" ", "-");
+  const anchor_id = document.createTextNode(anchor.id);
+  console.log(anchor_id)
   anchor.className = 'list-group-item list-group-item-action';
+  anchor.onclick(copy_text_to_clipboard(anchor_id))
   const text = document.createTextNode(last_pair);
   anchor.appendChild(text);
   document.getElementById("word_list_group").appendChild(anchor);
