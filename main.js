3714b0b565885dcb44e2b46f89d5d941b2cb7bed
Taco <taco-tues@tuta.io>
2019-11-20 13:33:53 -0700

    add random_index function

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const adjectives =  words.adjectives;
 const nouns      =  words.nouns;
 
 let historical_pairs = [];
+
+const random_index = (array) =>  { return Math.floor(Math.random() * array.length) };
