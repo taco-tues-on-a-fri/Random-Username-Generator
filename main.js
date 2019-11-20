089727109be9b140c424fd31fb1bd0b045202d6f
Taco <taco-tues@tuta.io>
2019-11-20 16:21:06 -0700

    refactored list+generate function

--- main.js
+++ main.js
@@ -4,40 +4,24 @@ const nouns      =  words.nouns;
 const random_index = (array) =>  { return Math.floor(Math.random() * array.length) };
 
 const create_anchor_list_item_w_last_pair = () => {
-  const last_pair = document.getElementById("random_name_textarea").innerHTML
+  const last_pair = document.getElementById("random_name_textarea").innerHTML;
   const anchor = document.createElement("A");
   anchor.id = last_pair.replace(" ", "-");
   anchor.className = 'list-group-item list-group-item-action';
   const text = document.createTextNode(last_pair);
   anchor.appendChild(text);
   document.getElementById("word_list_group").appendChild(anchor);
-}
+};
 
 const random_name_generator = () => {
   let random_pair = [];
-
   random_pair.push(adjectives[random_index(adjectives)]);
   random_pair.push(nouns[random_index(nouns)]);
-
   document.getElementById("random_name_textarea").innerHTML = random_pair.join(" ");
-}
+};
 
 const add_to_list_then_generate_new_pair = () => {
-  // const last_pair = document.getElementById("random_name_textarea").innerHTML
-  // const anchor = document.createElement('A');
-  // anchor.id = last_pair
-  // anchor.className = 'list-group-item list-group-item-action';
-  // const text = document.createTextNode(last_pair);
-  // anchor.appendChild(text);
-  // document.getElementById("word_list_group").appendChild(anchor);
-
   create_anchor_list_item_w_last_pair()
   random_name_generator()
-  // let random_pair = [];
-  
-  // random_pair.push(adjectives[random_index(adjectives)]);
-  // random_pair.push(nouns[random_index(nouns)]);
-
-  // document.getElementById("random_name_textarea").innerHTML = random_pair.join(" ");
-}
+};
 
