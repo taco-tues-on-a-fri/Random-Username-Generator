8e9e0dcf800f090435a09584ae627ec51b5dc494
Taco <taco-tues@tuta.io>
2019-11-20 17:27:28 -0700

    taking a different approach from here

--- main.js
+++ main.js
@@ -3,9 +3,54 @@ const nouns      =  words.nouns;
 
 const random_index = (array) =>  { return Math.floor(Math.random() * array.length) };
 
+var buttons = document.querySelectorAll('.js-button');
+
+var displayContent = function (button, content) {
+  if (content.classList.contains('active')) {
+      // Hide content
+      content.classList.remove('active');
+      button.setAttribute('aria-expanded', 'false');
+      content.setAttribute('aria-hidden', 'true');
+    } else {
+      // Show content
+      content.classList.add('active');
+      button.setAttribute('aria-expanded', 'true');
+      content.setAttribute('aria-hidden', 'false');
+    }
+};
+
+[].forEach.call(buttons, function(button, index) {
+  // Content var
+  var content = button.nextElementSibling;
+  
+  // Set button attributes
+  button.setAttribute('id', 'button-' + index);
+  button.setAttribute('aria-expanded', 'false');
+  button.setAttribute('aria-controls', 'content-' + index);
+  
+  // Set content attributes
+  content.setAttribute('id', 'content-' + index);
+  content.setAttribute('aria-hidden', 'true');    
+  content.setAttribute('aria-labelledby', 'button-' + index);
+
+  button.addEventListener('click', function () {
+    displayContent(this, content);
+    return false;
+  }, false);
+  
+  button.addEventListener('keydown', function (event) {
+    // Handle 'space' key
+    if (event.which === 32) {
+      event.preventDefault();
+      displayContent(this, content);
+    }
+  }, false);
+  
+});  
+
+
 function copy_text_to_clipboard(last_pair) {
   /* Get the text field */
-  console.log('inside clipboard: ' + JSON.stringify(last_pair))
   var copy_text = last_pair
 
   /* Select the text field */
@@ -24,9 +69,13 @@ const create_anchor_list_item_w_last_pair = () => {
   const anchor = document.createElement("A");
   anchor.id = last_pair.replace(" ", "-");
   var anchor_id = document.createTextNode(anchor.id);
-  console.log(anchor_id)
+
+  // console.log(anchor_id)
+  
   anchor.className = 'list-group-item list-group-item-action';
-  anchor.onclick(copy_text_to_clipboard(last_pair))
+  anchor.role= "button"
+  // anchor.onclick(copy_text_to_clipboard(last_pair))
+  
   const text = document.createTextNode(last_pair);
   anchor.appendChild(text);
   document.getElementById("word_list_group").appendChild(anchor);
